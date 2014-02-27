819fda8cb742f700461984b0c48b6afa97839951
QuantumPhi <phi.quantum@gmail.com>
2014-02-27 21:20:31 +0000

    Added more specific filters

--- src/main/Filter.java
+++ src/main/Filter.java
@@ -3,10 +3,28 @@ package main;
 import java.util.ArrayList;
 
 public class Filter {
-    public static Data[] filter(Data[] set) {
-        ArrayList<Data> filteredSet = new ArrayList<Data>();
+    public static Data[] filterGeo(Data[] set) {
+        ArrayList<Data> filteredSet = new ArrayList<>();
         for(Data item : set)
-            if(!item.isRetweet && item.location != null)
+            if(item.location != null)
+                filteredSet.add(item);
+        
+        return filteredSet.toArray(new Data[filteredSet.size()]);
+    }
+    
+    public static Data[] filterGeoLoc(Data[] set) {
+        ArrayList<Data> filteredSet = new ArrayList<>();
+        for(Data item : set)
+            if(item.latitude != 0 || item.longitude != 0)
+                filteredSet.add(item);
+        
+        return filteredSet.toArray(new Data[filteredSet.size()]);
+    }
+    
+    public static Data[] filterRetweet(Data[] set) {
+        ArrayList<Data> filteredSet = new ArrayList<>();
+        for(Data item : set)
+            if(!item.isRetweet)
                 filteredSet.add(item);
         
         return filteredSet.toArray(new Data[filteredSet.size()]);
