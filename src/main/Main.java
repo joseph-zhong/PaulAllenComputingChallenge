28c9dddb2be4eeb49fcd3d38078e4360558e4264
Shreyas <skraman1999@gmail.com>
2014-02-27 20:40:57 -0800

    More throws.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,12 @@
 package main;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.text.ParseException;
 
 public class Main {
     
-    public static void main(String[] args) throws FileNotFoundException, ParseException {
+    public static void main(String[] args) throws FileNotFoundException, ParseException, IOException {
         Data[] data = Parser.parse("phailin_tweets.csv");
     }
 }
--- src/main/Parser.java
+++ src/main/Parser.java
@@ -9,24 +9,20 @@ import java.text.ParseException;
 /** A class built around a buffered reader which can sort and stuff. */
 public class Parser {
         
-    public static Data[] parse(String ref) throws FileNotFoundException, ParseException {
+    public static Data[] parse(String ref) throws FileNotFoundException, ParseException, IOException {
         BufferedReader reader;
         Data[] data = new Data[6671];
 
         reader = new BufferedReader(new FileReader(ref));
         
-        try {
-            reader.readLine(); // Header.
-            String line = null;
-            for (int i=0;i<6671;i++) {
-                line = reader.readLine();
-                
-                Data newData = new Data(line,i);
-                
-                data[i] = newData;
-            }
-        } catch (IOException e) {
-            System.out.println("IO Error: " + e);
+        reader.readLine(); // Header.
+        String line = null;
+        for (int i=0;i<6671;i++) {
+            line = reader.readLine();
+
+            Data newData = new Data(line,i);
+
+            data[i] = newData;
         }
         return data;
     }
