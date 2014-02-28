5b577519aa351241be1ab3b8aa8f42a43cb8e4df
Shreyas <skraman1999@gmail.com>
2014-02-27 18:22:41 -0800

    Added simple data storage class.

---
+++ src/main/Data.java
@@ -0,0 +1,22 @@
+package main;
+
+import java.util.ArrayList;
+
+public class Data {
+    
+    public long tweetId;
+    public String time;
+    public int userId;
+    public int followers;
+    public String accountLocation;
+    public String tweetSource;
+    public ArrayList<String> hashtags;
+    public double latitude;
+    public double longitude;
+    public String location;
+    public String url1;
+    public String expandedUrl1;
+    public boolean isRetweet;
+    public long originalTweetId;
+    public int retweets; // Not sure on this one...
+}
