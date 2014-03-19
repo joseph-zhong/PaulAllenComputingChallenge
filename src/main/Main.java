c87bea810e7a53fb7920c3e0f728485e9fb25e92
Shreyas <skraman1999@gmail.com>
2014-03-18 19:08:06 -0700

    Ignore Main when you commit to keep it clean.

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,18 +3,10 @@ package main;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.ParseException;
-import main.render.Renderer;
 
 public class Main {
     
     public static void main(String[] args) throws FileNotFoundException, ParseException, IOException {
         Data[] data = Parser.parse("phailin_tweets.csv");
-        Packet[] packet = Exporter.exportToPackets(data);
-        
-        System.out.println();
-        System.out.println(Exporter.exportTweetsOverTimeAscii(data,1,4));
-        Exporter.exportTweetsOverTimeCsv(data,1);
-        
-        Renderer r = new Renderer();
     }
 }
