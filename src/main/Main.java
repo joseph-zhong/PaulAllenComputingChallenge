fec84fda4863e85cc64d9167137220909a934c04
QuantumPhi <phi.quantum@gmail.com>
2014-03-27 19:20:04 -0700

    NEW DELHI.CSV

--- src/main/Main.java
+++ src/main/Main.java
@@ -17,12 +17,14 @@ public class Main {
             {"cyclonephailin","odisha","india","cyclone","news",
             "andhrapradesh","phailinfury","bhubaneswar","gopalpur","nari"};
         //Lat-Long for Gopalpur and Mumbai, respectively
-        Vector2D[] locs = new Vector2D[] { new Vector2D(84.92, 19.27), new Vector2D(72.8258, 18.975) };
+        Vector2D[] locs = new Vector2D[] { new Vector2D(84.92, 19.27), new Vector2D(72.8258, 18.975), new Vector2D(77.23, 28.61) };
         Data[] data = Parser.parse("phailin_tweets.csv");
         
         Exporter.exportTweetsOverTimeCsv(Filter.filterNearLocation(Filter.filterGeoLoc(data),locs[0]),
                 1,"gopalpur.csv",true);
         Exporter.exportTweetsOverTimeCsv(Filter.filterNearLocation(Filter.filterGeoLoc(data),locs[1]),
                 1,"mumbai.csv",true);
+        Exporter.exportTweetsOverTimeCsv(Filter.filterNearLocation(Filter.filterGeoLoc(data),locs[2]),
+                1,"new-delhi.csv",true);
     }
 }
